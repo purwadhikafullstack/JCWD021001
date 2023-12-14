7f40d69081a42253d13cdffb050cd4d1c5ca66f7
phillip-steven <putu.phillip@gmail.com>
2023-12-14 12:49:15 +0700

    feat(size): add size table

---
+++ apps/api/src/migrations/20231214054003-create-size.js
@@ -0,0 +1,30 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable('sizes', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: Sequelize.STRING,
+      unique: true,
+    },
+    productCategoryId: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      references: {
+        model: {
+          tableName: 'productCategories',
+        },
+        key: 'id',
+      },
+    },
+  });
+}
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable('sizes');
+}
