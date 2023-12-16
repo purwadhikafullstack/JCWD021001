2d9ffc8a4f51a24d28ca0fa15491510650358fd9
I Putu Phillip Steven <putu.phillip@gmail.com>
2023-12-16 13:59:18 +0700

    Jcwd021001 52 (#20) (#22)
    * feat(auth): add user-registration and email verification
    
    * feat(auth): add login and keep-login
    
    * feat(auth): add request reset password
    
    * feat(initialization): install some package
    
    * feat(productImage): add productImage table
    
    * feat(mutationStatus): add mutationStatus table
    
    * feat(mutation): add mutation table
    
    * feat(stockReport): add stockReport table
    
    ---------
    
    Co-authored-by: mhmdRfi <mahmudrofiie@gmail.com>
    Co-authored-by: mhmdRfi <143689582+mhmdRfi@users.noreply.github.com>
    Co-authored-by: andrianggoro21 <andri.anggoro21@gmail.com>

---
+++ apps/api/src/models/productImage.model.js
@@ -0,0 +1,31 @@
+import { Model, DataTypes } from 'sequelize';
+
+export default class ProductImage extends Model {
+  /**
+   * Helper method for defining associations.
+   * This method is not a part of Sequelize lifecycle.
+   * The `models/index` file will call this method automatically.
+   */
+  static associate(models) {
+    // define association here
+  }
+}
+
+export const init = (sequelize) => {
+  ProductImage.init(
+    {
+      imageUrl: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      productId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'ProductImage',
+    },
+  );
+};
