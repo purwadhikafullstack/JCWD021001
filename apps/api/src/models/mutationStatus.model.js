b94fcc4d10edd482bffb63c0185f33057af2308d
phillip-steven <putu.phillip@gmail.com>
2023-12-16 09:16:23 +0700

    feat(mutationStatus): add mutationStatus table

---
+++ apps/api/src/models/mutationStatus.model.js
@@ -0,0 +1,29 @@
+'use strict';
+import { Model, DataTypes } from 'sequelize';
+export default class MutationStatus extends Model {
+  /**
+   * Helper method for defining associations.
+   * This method is not a part of Sequelize lifecycle.
+   * The `models/index` file will call this method automatically.
+   */
+  static associate(models) {
+    // define association here
+  }
+}
+MutationStatus.init(
+  {
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isAlpha: true,
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: 'MutationStatus',
+  },
+);
+return MutationStatus;
