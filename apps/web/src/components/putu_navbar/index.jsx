eaf591c4b257c5572d0f290fe3dfa18e66085636
phillip-steven <putu.phillip@gmail.com>
2024-01-01 22:21:53 +0700

    feat(navbar): add temporary navbar mobile design

--- apps/web/src/components/putu_navbar/index.jsx
+++ apps/web/src/components/putu_navbar/index.jsx
@@ -1,111 +1,43 @@
 import {
   Box,
-  Button,
+  Divider,
   Flex,
-  Grid,
+  HStack,
   Icon,
   Image,
-  Input,
-  InputGroup,
-  InputRightElement,
+  Spacer,
+  Text,
+  Center,
+  Select,
 } from '@chakra-ui/react';
-import logo from '../../assets/images/logo.png';
 import {
   MagnifyingGlassIcon,
   ShoppingCartIcon,
-  HeartIcon,
+  BellIcon,
 } from '@heroicons/react/24/outline';
+import pure from '/logo/pure.png';
 
 function PutuNavbar() {
   return (
-    <Box>
-      <Flex
-        className="navbar-top"
-        padding={'6px'}
-        // backgroundColor={'brand.lightred'}
-        alignItems={'center'}
-        // width={'1440px'}
-        height={'82px'}
-        justifyContent={'space-between'}
-        boxShadow={'base'}
-      >
-        <Grid
-          className="Navbar-left"
-          gridTemplateColumns={'1fr auto'}
-          gridGap={'29px'}
-          padding={'16px'}
-          alignItems={'center'}
-        >
-          <Image boxSize={'53.195px'} src={logo} alt="Logo" />
-          <InputGroup>
-            <Input
-              height={'40px'}
-              maxWidth={'443px'}
-              placeholder="Search a product here"
-              borderRadius="8px"
-              border="1px solid #C7C7C7"
-              background={'white'}
-            />
-            <InputRightElement>
-              <Icon as={MagnifyingGlassIcon} color={'brand.grey300'} />
-            </InputRightElement>
-          </InputGroup>
-        </Grid>
-        <Flex
-          className="Navbar-left"
-          padding={'16px'}
-          alignItems={'center'}
-          justifyContent={'space-between'}
-        >
-          <Grid
-            gridGap={'36px'}
-            padding={'16px'}
-            gridTemplateColumns={'1fr 1fr'}
-            borderRight={'1px solid #D3D0D0'}
-          >
-            <Icon
-              as={ShoppingCartIcon}
-              color={'brand.grey300'}
-              boxSize={'24px'}
-            />
-            <Icon as={HeartIcon} color={'brand.grey300'} boxSize={'24px'} />
-          </Grid>
-          <Grid
-            gridGap={'16px'}
-            padding={'16px'}
-            gridTemplateColumns={'1fr 1fr'}
-          >
-            <Button
-              color="brand.lightred"
-              borderColor={'brand.lightred'}
-              variant={'outline'}
-              width={'88px'}
-              padding={'9px 11px'}
-              _hover={{
-                opacity: '80%',
-              }}
-              _active={{
-                opacity: '50%',
-              }}
-            >
-              Sign Up
-            </Button>
-            <Button
-              color="white"
-              bg={'brand.lightred'}
-              width={'88px'}
-              padding={'9px 11px'}
-              _hover={{
-                opacity: '80%',
-              }}
-              _active={{
-                opacity: '50%',
-              }}
-            >
-              Sign In
-            </Button>
-          </Grid>
-        </Flex>
+    <Box p={'.5em 1em'}>
+      <Flex alignItems={'center'} justifyContent={'space-between'}>
+        <Box h={'auto'} w={'3em'}>
+          <Image src={pure} alt="Logo" />
+        </Box>
+        <Spacer />
+        <HStack>
+          <HStack fontSize={'1.5em'} spacing={'.5em'}>
+            <Icon as={MagnifyingGlassIcon} />
+            <Icon as={ShoppingCartIcon} />
+            <Icon as={BellIcon} />
+          </HStack>
+          <Center height="2em">
+            <Divider orientation="vertical" borderWidth={'1px'} />
+          </Center>
+          <Select w={'5em'} variant={'unstyled'}>
+            <option>Sign In</option>
+          </Select>
+        </HStack>
       </Flex>
     </Box>
   );
