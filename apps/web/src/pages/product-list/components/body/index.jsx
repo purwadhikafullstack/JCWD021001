8494a9af9a46f95691c0b0838c6608d3fdf92612
phillip-steven <putu.phillip@gmail.com>
2024-01-01 22:23:29 +0700

    feat(product-list): add product-list pages

---
+++ apps/web/src/pages/product-list/components/body/index.jsx
@@ -0,0 +1,9 @@
+import { Box, Text } from '@chakra-ui/react';
+
+export const Body = () => {
+  return (
+    <Box p={'.5em 1em'} bgColor={'grey.50'}>
+      <Text fontWeight={'bold'}>Women</Text>
+    </Box>
+  );
+};
---
+++ apps/web/src/pages/product-list/container/index.jsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react';
+import PutuNavbar from '../../../components/putu_navbar';
+import { Body } from '../components/body';
+
+export const Product = () => {
+  return (
+    <Box>
+      <PutuNavbar />
+      <Body />
+    </Box>
+  );
+};
