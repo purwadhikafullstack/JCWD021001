604336b799461e9403703a8627f32ef7ecefc589
phillip-steven <putu.phillip@gmail.com>
2024-01-04 10:22:19 +0700

    fix(putu_navbar): fixing export bug on navbar

--- apps/web/src/components/putu_navbar/index.jsx
+++ apps/web/src/components/putu_navbar/index.jsx
@@ -6,7 +6,6 @@ import {
   Icon,
   Image,
   Spacer,
-  Text,
   Center,
   Select,
 } from '@chakra-ui/react';
@@ -14,33 +13,43 @@ import {
   MagnifyingGlassIcon,
   ShoppingCartIcon,
   BellIcon,
+  XMarkIcon,
 } from '@heroicons/react/24/outline';
 import pure from '/logo/pure.png';
 
-function PutuNavbar() {
+export const PutuNavbar = (props) => {
   return (
     <Box p={'.5em 1em'}>
-      <Flex alignItems={'center'} justifyContent={'space-between'}>
-        <Box h={'auto'} w={'3em'}>
-          <Image src={pure} alt="Logo" />
-        </Box>
-        <Spacer />
-        <HStack>
-          <HStack fontSize={'1.5em'} spacing={'.5em'}>
-            <Icon as={MagnifyingGlassIcon} />
-            <Icon as={ShoppingCartIcon} />
-            <Icon as={BellIcon} />
+      <Box>
+        <Flex alignItems={'center'} justifyContent={'space-between'}>
+          <Box h={'auto'} w={'3em'}>
+            <Image src={pure} alt="Logo" />
+          </Box>
+          <Spacer />
+          <HStack visibility={props?.collapseSideBar ? 'hidden' : 'visible'}>
+            <HStack fontSize={'1.5em'} spacing={'.5em'}>
+              <Icon as={MagnifyingGlassIcon} />
+              <Icon as={ShoppingCartIcon} />
+              <Icon as={BellIcon} />
+            </HStack>
+            <Center height="2em">
+              <Divider orientation="vertical" borderWidth={'1px'} />
+            </Center>
+            <Select w={'5em'} variant={'unstyled'}>
+              <option>Sign In</option>
+            </Select>
           </HStack>
-          <Center height="2em">
-            <Divider orientation="vertical" borderWidth={'1px'} />
-          </Center>
-          <Select w={'5em'} variant={'unstyled'}>
-            <option>Sign In</option>
-          </Select>
-        </HStack>
-      </Flex>
+          <Box display={props?.collapseSideBar ? 'block' : 'none'}>
+            <Flex justifyContent={'center'} alignItems={'center'}>
+              <Icon
+                as={XMarkIcon}
+                fontSize={'1.3em'}
+                onClick={() => props?.toggleSideBar()}
+              />
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
     </Box>
   );
-}
-
-export default PutuNavbar;
+};
