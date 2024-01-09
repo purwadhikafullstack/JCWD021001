fe5585628aa2262f7b72f3b9c4a12449c48cd25b
phillip-steven <putu.phillip@gmail.com>
2024-01-09 18:00:19 +0700

    fix(readProduct): add setSortby and setOrderBy arguments

--- apps/web/src/pages/product-list/services/readProduct.js
+++ apps/web/src/pages/product-list/services/readProduct.js
@@ -6,10 +6,12 @@ export const getProduct = async (
   productType = '',
   productCategory = '',
   setProduct,
+  sortBy,
+  orderBy,
 ) => {
   try {
     const res = await axios.get(
-      `http://localhost:8000/api/product?name=${name}&productGroup=${productGroup}&productType=${productType}&productCategory=${productCategory}`,
+      `http://localhost:8000/api/product?name=${name}&productGroup=${productGroup}&productType=${productType}&productCategory=${productCategory}&sortBy=${sortBy}&orderBy=${orderBy}`,
     );
     setProduct(res?.data?.data);
   } catch (err) {
