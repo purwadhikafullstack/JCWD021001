e6fe58415bc04bea6535ac39093950cdbf21d8ef
andrianggoro21 <andri.anggoro21@gmail.com>
2024-01-10 15:01:42 +0700

    fix(UI): create ui cart product (progress)

--- apps/web/src/pages/cart/Index.jsx
+++ apps/web/src/pages/cart/Index.jsx
@@ -2,13 +2,34 @@ import { Box, Text } from '@chakra-ui/react'
 import Navbar from '../../components/Navbar/Navbar'
 import LaptopCartTable from '../../components/cart-table/laptopCartTable'
 import MobileCartTable from '../../components/cart-table/mobileCartTable'
+import { useState, useEffect } from 'react'
+import { getCart } from './services/getCart'
 
 const Cart = () => {
+  const [cartData, setCartData] = useState([])
+
+  const refreshCart = async () => {
+    try {
+      const data = await getCart();
+      setCartData(data);
+    } catch (error) {
+      console.error('Error fetching cart data:', error);
+    }
+  };
+
+  const handleCartUpdated = () => {
+    refreshCart();
+  };
+
+  useEffect(() => {
+    refreshCart();
+  }, []);
+  
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Box bgColor={'brand.grey100'} maxW={'100vw'} minH={'100vh'}>
-        <LaptopCartTable />
+        <LaptopCartTable cartData={cartData} onCartUpdated={handleCartUpdated}/>
         <MobileCartTable />
       </Box>
     </>
