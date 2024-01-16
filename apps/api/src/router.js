79227c244c58a4f4d91e2d4e4b56692635a42d8b
phillip-steven <putu.phillip@gmail.com>
2024-01-16 11:35:39 +0700

    fix(router): remove unnecessary route

--- apps/api/src/router.js
+++ apps/api/src/router.js
@@ -1,32 +1,31 @@
-import { Router } from 'express';
-import { sampleRouter } from './routers/sample.router';
-import { authRouter } from './routers/auth.router';
-import { cartRouter } from './routers/carts.router';
-import { orderRouter } from './routers/orders.router';
-import { productRouter } from './routers/product.router';
-import { userRouter } from './routers/user.router';
-import {userAddressRouter} from './routers/userAddress.router'
+import { Router } from 'express'
+import { sampleRouter } from './routers/sample.router'
+import { authRouter } from './routers/auth.router'
+import { cartRouter } from './routers/carts.router'
+import { orderRouter } from './routers/orders.router'
+import { productRouter } from './routers/product.router'
+import { userRouter } from './routers/user.router'
+import { userAddressRouter } from './routers/userAddress.router'
 
-
-import { paymentRouter } from './routers/payments.router';
-import { stockRouter } from './routers/stock.router';
-import { productCategoryRouter } from './routers/productCategory.router';
-const router = Router();
+import { paymentRouter } from './routers/payments.router'
+import { stockRouter } from './routers/stock.router'
+import { productCategoryRouter } from './routers/productCategory.router'
+const router = Router()
 
 router.get('/', (req, res) => {
-  res.send(`Hello, Purwadhika Student !`);
-});
+  res.send(`Hello, Purwadhika Student !`)
+})
 
-router.use('/sample', sampleRouter);
+router.use('/sample', sampleRouter)
 
 // add another router here ...
-router.use('/auth', authRouter);
-router.use('/cart', cartRouter);
-router.use('/order', orderRouter);
-router.use('/product', productRouter);
-router.use('/user', userRouter);
-router.use('/user-address', userAddressRouter);
-router.use('/payment', paymentRouter);
-router.use('/stock', stockRouter);
-router.use('/product-category', productCategoryRouter);
-export default router;
+router.use('/auth', authRouter)
+router.use('/cart', cartRouter)
+router.use('/order', orderRouter)
+router.use('/product', productRouter)
+router.use('/user', userRouter)
+router.use('/user-address', userAddressRouter)
+router.use('/payment', paymentRouter)
+router.use('/stock', stockRouter)
+router.use('/product-category', productCategoryRouter)
+export default router
