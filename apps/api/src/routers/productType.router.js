8e938e953d28d1a803063195367bfe72becbe2a4
phillip-steven <putu.phillip@gmail.com>
2024-01-16 11:31:00 +0700

    fix(productType): delete unnecessary files

--- apps/api/src/routers/productType.router.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { getProductTypeController } from '../controllers/productType.controller'
-const productTypeRouter = Router()
-
-productTypeRouter.get('/', getProductTypeController)
-
-export { productTypeRouter }
