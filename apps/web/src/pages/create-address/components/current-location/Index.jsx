a496f871ee68ed83504d25937e1b35fec5f591ce
mhmdRfi <mahmudrofiie@gmail.com>
2024-01-30 14:34:52 +0700

    (feat) add find nearest warehouse and shipping cost front end

--- apps/web/src/pages/create-address/components/current-location/Index.jsx
+++ apps/web/src/pages/create-address/components/current-location/Index.jsx
@@ -84,32 +84,32 @@ function FormCurrentLocation ({ address, lat, lng }) {
                         mb={'8px'}>
                             Full Name
                         </Text>
-                            <Input
-                            name="fullName"
-                            placeholder="Type your full name here"
-                            _placeholder={{color: 'brand.grey350'}}
-                            bg={'brand.grey100'}
-                            variant={'filled'}
-                            mb={'32px'}
-                            value={formik.values.fullName}
-                            onChange={formik.handleChange}/>
+                        <Input
+                        name="fullName"
+                        placeholder="Type your full name here"
+                        _placeholder={{color: 'brand.grey350'}}
+                        bg={'brand.grey100'}
+                        variant={'filled'}
+                        mb={'32px'}
+                        value={formik.values.fullName}
+                        onChange={formik.handleChange}/>
                     </Box>
                     <Box>  
-                            <Text fontSize={'16px'}
-                            fontWeight={'700'}
-                            color={'brand.grey350'}
-                            mb={'8px'}>
-                                Mobile Phone
-                            </Text>
-                            <Input
-                            placeholder="Type your mobile phone number here"
-                            _placeholder={{color: 'brand.grey350'}}
-                            bg={'brand.grey100'}
-                            variant={'filled'} 
-                            mb={'24px'}
-                            name="phoneNumber"
-                            value={formik.values.phoneNumber}
-                            onChange={formik.handleChange}/>       
+                        <Text fontSize={'16px'}
+                        fontWeight={'700'}
+                        color={'brand.grey350'}
+                        mb={'8px'}>
+                            Mobile Phone
+                        </Text>
+                        <Input
+                        placeholder="Type your mobile phone number here"
+                        _placeholder={{color: 'brand.grey350'}}
+                        bg={'brand.grey100'}
+                        variant={'filled'} 
+                        mb={'24px'}
+                        name="phoneNumber"
+                        value={formik.values.phoneNumber}
+                        onChange={formik.handleChange}/>       
                     </Box>
                 </Grid>
                 <Text 
@@ -124,83 +124,83 @@ function FormCurrentLocation ({ address, lat, lng }) {
                 gridTemplateColumns={'1fr 1fr'}>
                     <Box>
                         
-                            <Text fontSize={'16px'}
-                            fontWeight={'700'}
-                            color={'brand.grey350'}
-                            mb={'8px'}>
-                                Province
-                            </Text>
-                            <Select
-                            placeholder="Select a Province"
-                            bg={'brand.grey100'}
-                            variant={'filled'} 
-                            color={'brand.grey350'}
-                            mb={'24px'}
-                            value={selectedProvince}
-                            onChange={(e) => setSelectedProvince(e.target.value)}>
-                                {provinceList?.map((province) => (
-                                    <option key={province.id} value={province.id}>
-                                        {province.name}
-                                    </option>
-                                ))}
-                            </Select>
-                            <Text fontSize={'16px'}
-                            fontWeight={'700'}
-                            color={'brand.grey350'}
-                            mb={'8px'}>
-                                City
-                            </Text>
-                            <Select 
-                            value={selectedCity}
-                            placeholder="Select a City"
-                            bg={'brand.grey100'}
-                            color={'brand.grey350'}
-                            variant={'filled'} 
-                            mb={'24px'}
-                            name="cityId"
-                            onChange={(e) => {
-                                setSelectedCity(e.target.value);
-                                formik.handleChange(e);
-                            }}>
-                                {citylist?.map((city) => (
-                                    <option key={city.id} 
-                                    value={city.id}
-                                    >
-                                        {city.name}
-                                    </option>
-                                ))}
-                            </Select>
-                            <Text fontSize={'16px'}
-                            fontWeight={'700'}
-                            color={'brand.grey350'}
-                            mb={'8px'}>
-                                Postal Code
-                            </Text>
-                            <Input
-                            placeholder="Type a postal code"
-                            _placeholder={{color: 'brand.grey350'}}
-                            bg={'brand.grey100'}
-                            variant={'filled'} 
-                            mb={'24px'}
-                            name="postalCode"
-                            value={formik.values.postalCode}
-                            onChange={formik.handleChange}/>
+                        <Text fontSize={'16px'}
+                        fontWeight={'700'}
+                        color={'brand.grey350'}
+                        mb={'8px'}>
+                            Province
+                        </Text>
+                        <Select
+                        placeholder="Select a Province"
+                        bg={'brand.grey100'}
+                        variant={'filled'} 
+                        color={'brand.grey350'}
+                        mb={'24px'}
+                        value={selectedProvince}
+                        onChange={(e) => setSelectedProvince(e.target.value)}>
+                            {provinceList?.map((province) => (
+                                <option key={province.id} value={province.id}>
+                                    {province.name}
+                                </option>
+                            ))}
+                        </Select>
+                        <Text fontSize={'16px'}
+                        fontWeight={'700'}
+                        color={'brand.grey350'}
+                        mb={'8px'}>
+                            City
+                        </Text>
+                        <Select 
+                        value={selectedCity}
+                        placeholder="Select a City"
+                        bg={'brand.grey100'}
+                        color={'brand.grey350'}
+                        variant={'filled'} 
+                        mb={'24px'}
+                        name="cityId"
+                        onChange={(e) => {
+                            setSelectedCity(e.target.value);
+                            formik.handleChange(e);
+                        }}>
+                            {citylist?.map((city) => (
+                                <option key={city.id} 
+                                value={city.id}
+                                >
+                                    {city.name}
+                                </option>
+                            ))}
+                        </Select>
+                        <Text fontSize={'16px'}
+                        fontWeight={'700'}
+                        color={'brand.grey350'}
+                        mb={'8px'}>
+                            Postal Code
+                        </Text>
+                        <Input
+                        placeholder="Type a postal code"
+                        _placeholder={{color: 'brand.grey350'}}
+                        bg={'brand.grey100'}
+                        variant={'filled'} 
+                        mb={'24px'}
+                        name="postalCode"
+                        value={formik.values.postalCode}
+                        onChange={formik.handleChange}/>
                     </Box>
                     <Box>   
-                            <Text fontSize={'16px'}
-                            fontWeight={'700'}
-                            color={'brand.grey350'}
-                            mb={'8px'}>
-                                Address (Ex : Street, Residence, number of house)
-                            </Text>
-                            <Textarea placeholder="Type your address"
-                            name="specificAddress"
-                            _placeholder={{color: 'brand.grey350'}}
-                            bg={'brand.grey100'}
-                            variant={'filled'} 
-                            h={'210px'}
-                            value={formik.values.specificAddress}
-                            onChange={formik.handleChange}/>
+                        <Text fontSize={'16px'}
+                        fontWeight={'700'}
+                        color={'brand.grey350'}
+                        mb={'8px'}>
+                            Address (Ex : Street, Residence, number of house)
+                        </Text>
+                        <Textarea placeholder="Type your address"
+                        name="specificAddress"
+                        _placeholder={{color: 'brand.grey350'}}
+                        bg={'brand.grey100'}
+                        variant={'filled'} 
+                        h={'210px'}
+                        value={formik.values.specificAddress}
+                        onChange={formik.handleChange}/>
                     </Box>
                 </Grid>
                 <Flex justifyContent={'flex-end'}
